795af9ce7f74b48c2f944568b268cbe03f45c605
wanyimu <13307130174@fudan.edu.cn>
2015-08-06 20:06:42 +0800

    Upload Demo
    根据官方Demo重写了一遍，测试了一下，效果还不错。重写的过程可以熟悉其不同组件。

---
+++ demo/IndexFiles.java
@@ -0,0 +1,125 @@
+package org.apache.lucene.demo;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.LongField;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Date;
+
+public class IndexFiles {
+	private IndexFiles() {
+	}
+
+	public static void main(String[] args) {
+		String usage = "java org.apache.lucene.demo.IndexFiles" + " [-index INDEX_PATH] [-docs DOCS_PATH] [-update]\n\n"
+				+ "This indexes the documents in DOCS_PATH, creating a Lucene index"
+				+ "in INDEX_PATH that can be searched with SearchFiles";
+		String indexPath = "index";
+		String docsPath = null;
+		boolean create = true;
+		for (int i = 0; i < args.length; ++i) {
+			if ("-index".equals(args[i])) {
+				indexPath = args[i + 1];
+				i++;
+			} else if ("-docs".equals(args[i])) {
+				docsPath = args[i + 1];
+				i++;
+			} else if ("-update".equals(args[i])) {
+				create = false;
+			}
+		}
+
+		if (docsPath == null) {
+			System.err.println("Usage:" + usage);
+			System.exit(1);
+		}
+
+		final Path docDir = Paths.get(docsPath);
+		if (!Files.isReadable(docDir)) {
+			System.out.println("Document directory '" + docDir.toAbsolutePath()
+					+ "' does not exist or is not readable, please check the path.");
+			System.exit(1);
+		}
+
+		Date start = new Date();
+		try {
+			System.out.println("Indexing to directory '" + indexPath + "' ...");
+
+			Directory dir = FSDirectory.open(Paths.get(indexPath));
+			Analyzer analyzer = new StandardAnalyzer();
+			IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+			if (create) {
+				iwc.setOpenMode(OpenMode.CREATE);
+			} else {
+				iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+			}
+			IndexWriter writer = new IndexWriter(dir, iwc);
+			indexDocs(writer, docDir);
+
+			writer.close();
+			Date end = new Date();
+			System.out.println(end.getTime() - start.getTime() + " total milliseconds");
+		} catch (IOException e) {
+			System.out.println(" caught a " + e.getClass() + " \n with message: " + e.getMessage());
+		}
+	}
+
+	static void indexDocs(final IndexWriter writer, Path path) throws IOException {
+		if (Files.isDirectory(path)) {
+			Files.walkFileTree(path, new SimpleFileVisitor<Path>() {
+				@Override
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+					try {
+						indexDoc(writer, file, attrs.lastModifiedTime().toMillis());
+					} catch (IOException ignore) {
+						// don't index files that can't be read.
+					}
+					return FileVisitResult.CONTINUE;
+				}
+			});
+		} else {
+			indexDoc(writer, path, Files.getLastModifiedTime(path).toMillis());
+		}
+	}
+
+	static void indexDoc(IndexWriter writer, Path file, long lastModified) throws IOException {
+		try (InputStream stream = Files.newInputStream(file)) {
+			Document doc = new Document();
+			Field pathField = new StringField("path", file.toString(), Field.Store.YES);
+			doc.add(pathField);
+			doc.add(new LongField("modified", lastModified, Field.Store.NO));
+			doc.add(new TextField("contents",
+					new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
+			
+			if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
+				System.out.println("adding " + file);
+				writer.addDocument(doc);
+			} else {
+				System.out.println("updating " + file);
+				writer.updateDocument(new Term("path", file.toString()), doc);
+			}
+		}
+
+	}
+}
---
+++ demo/Readme.md
@@ -0,0 +1,56 @@
+## Demo
+
+
+该演示代码是重写的官方演示代码。
+
+官方代码地址为:
+
+[IndexFiles.java](https://lucene.apache.org/core/5_2_1/demo/src-html/org/apache/lucene/demo/IndexFiles.html)
+
+[SearchFiles.java](https://lucene.apache.org/core/5_2_1/demo/src-html/org/apache/lucene/demo/SearchFiles.html)
+
+---
+
+#### 使用说明
+
+以Eclipse为集成开发环境。  
+首先，需要下载[*Lucene 5.2.1*](http://lucene.apache.org)（Maven导入也可），导入以下jar包：
+
+* lucene-core-5.2.1.jar
+* lucene-analyzers-common-5.2.1.jar
+* lucene-queryparser-5.2.1.jar
+
+创建工程，写入这两个文件。  
+目录自定，此处参考官方代码，设置包为org.apache.lucene.demo。[^package]
+
+[^package]:此时的文件目录即为src/org/apache/lucene/demo, 目录不符合时Eclipse会自动提示移动文件.
+
+* **IndexFiles**	创建索引  
+* **SearchFiles**	查询索引
+
+
+两个文件各有一个main函数，执行时，需要携带参数表args.  
+可以在Run菜单中，选择Run Configurations, main class指定IndexFiles或SearchFiles[^1]  
+在Arguments中，输入Program Arguments后执行。
+
+[^1]: 官方代码中，文件被打包在org.apache.lucene.demo中，则需要指定完整的包目录，如org.apache.lucene.demo.IndexFiles.
+
+IndexFile参数表格式如下：
+
+    java org.apache.lucene.demo.IndexFiles [-index INDEX_PATH] [-docs DOCS_PATH] [-update]
+    
+    // The document in DOCS_PATH, creating a Lucene index in INDEX_PATH that can be searched with SearchFiles.
+    // e.g.: java IndexFiles -index D:\LuceneDemo\Index -docs D:\LuceneDemo\Data
+    
+SearchFile参数表格式如下：
+
+	java org.apache.lucene.demo.SearchFiles [-index dir] [-field f] [-repeat n] [-queries file] [-query string] [-raw] [-paging hitsPerPage]
+	
+	//See http://lucene.apache.org/core/5_2_1/demo for details.
+	// e.g.: java SearchFiles -index D:\LuceneDemo\Index -query Demo
+	
+	
+执行后，即可在命令行输出中看到当前状态或查询结果。
+
+2015年8月6日  
+©copyright 慕瑜
